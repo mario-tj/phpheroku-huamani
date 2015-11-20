3661c761346ee8e8bacb2b82308ab08dc1e9edcf
Juan-P <jpintado7603@gmail.com>
2015-11-20 12:15:42 -0500

    agregando tabla

--- index.php
+++ index.php
@@ -8,5 +8,18 @@
 </head>
 <body>
 	<h1>bienvenido a heroku</h1>
+	<table>
+		<caption>table title and/or explanatory text</caption>
+		<thead>
+			<tr>
+				<th>header</th>
+			</tr>
+		</thead>
+		<tbody>
+			<tr>
+				<td>data</td>
+			</tr>
+		</tbody>
+	</table>
 </body>
 </html>
